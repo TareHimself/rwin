95edc26b5114da2a8d5e35f03d93a2ebe2f39ac9
Oyintare Ebelo <75279482+TareHimself@users.noreply.github.com>
2025-08-13 14:42:56 -0700

    update

--- lib/rwin/windows/WindowsWindowManager.cpp
+++ lib/rwin/windows/WindowsWindowManager.cpp
@@ -1,5 +1,15 @@
-﻿#include "WindowsWindowManager.h"
+﻿#include "rwin/macros.h"
+
 #ifdef RWIN_PLATFORM_WIN
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0A00 // Windows 10
+#endif
+
+#ifndef NTDDI_VERSION
+#define NTDDI_VERSION 0x0A000000
+#endif
+
+#include "WindowsWindowManager.h"
 #include <windows.h>
 #include <shlobj.h>
 #include <combaseapi.h>
